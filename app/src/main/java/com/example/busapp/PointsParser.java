f70d5522b730ad88459eabb2c52c51035077fa5a
anujchoudhury <anuj.choudhury27@gmail.com>
2019-04-21 17:34:11 +0530

    Initial commit

---
+++ app/src/main/java/com/example/busapp/PointsParser.java
@@ -0,0 +1,96 @@
+package com.example.busapp;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.os.AsyncTask;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.model.Dash;
+import com.google.android.gms.maps.model.Dot;
+import com.google.android.gms.maps.model.Gap;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.PatternItem;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+class PointsParser extends AsyncTask<String, Integer, List<List<HashMap<String, String>>>> {
+    // Polyline options
+    public static final int PATTERN_DASH_LENGTH_PX = 20;
+    public static final int PATTERN_GAP_LENGTH_PX = 20;
+    public static final PatternItem DOT = new Dot();
+    public static final PatternItem DASH = new Dash(PATTERN_DASH_LENGTH_PX);
+    public static final PatternItem GAP = new Gap(PATTERN_GAP_LENGTH_PX);
+    public static final List<PatternItem> PATTERN_POLYGON_ALPHA = Arrays.asList(GAP, DOT);
+
+    TaskLoadedCallback taskCallback;
+    String directionMode = "transit";
+
+    public PointsParser(Context mContext, String directionMode) {
+        this.taskCallback = (TaskLoadedCallback) mContext;
+        this.directionMode = directionMode;
+    }
+
+    // Parsing the data in non-ui thread
+    @Override
+    protected List<List<HashMap<String, String>>> doInBackground(String... jsonData) {
+
+        JSONObject jObject;
+        List<List<HashMap<String, String>>> routes = null;
+
+        try {
+            jObject = new JSONObject(jsonData[0]);
+            DataParser parser = new DataParser();
+
+            // Starts parsing data
+            routes = parser.parse(jObject);
+        } catch (Exception e) {
+        }
+        return routes;
+    }
+
+    // Executes in UI thread, after the parsing process
+    @Override
+    protected void onPostExecute(List<List<HashMap<String, String>>> result) {
+        ArrayList<LatLng> points;
+        PolylineOptions lineOptions = null;
+        // Traversing through all the routes
+        for (int i = 0; i < result.size(); i++) {
+            points = new ArrayList<>();
+            lineOptions = new PolylineOptions();
+            // Fetching i-th route
+            List<HashMap<String, String>> path = result.get(i);
+            // Fetching all the points in i-th route
+            for (int j = 0; j < path.size(); j++) {
+                HashMap<String, String> point = path.get(j);
+                double lat = Double.parseDouble(point.get("lat"));
+                double lng = Double.parseDouble(point.get("lng"));
+                LatLng position = new LatLng(lat, lng);
+                points.add(position);
+            }
+            // Adding all the points in the route to LineOptions
+            lineOptions.addAll(points);
+            if (directionMode.equalsIgnoreCase("walking")) {
+                lineOptions.width(10);
+                lineOptions.pattern(PATTERN_POLYGON_ALPHA);
+                lineOptions.color(Color.MAGENTA);
+            } else {
+                lineOptions.width(20);
+                lineOptions.color(Color.argb(128, 0, 0, 255));
+            }
+        }
+
+        // Drawing polyline in the Google Map for the i-th route
+        if (lineOptions != null) {
+            //mMap.addPolyline(lineOptions);
+            taskCallback.onTaskDone(lineOptions);
+
+        } else {
+        }
+    }
+}
